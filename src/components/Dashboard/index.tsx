41f8562a9302a32059ab60e1dad0f9b3cd5e22a6
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-02-07 20:20:41 -0300

    feat: criando os componentes: Header e Dashboard(Summary)

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Summary } from "../Summary";
+
+import { Container } from "./styles";
+
+export function DashBoard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  )
+}
