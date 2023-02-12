f5940ac17aa81d5e4f54951fddd27783cb1b2941
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-02-12 15:02:29 -0300

    feat: botão de tipo funcional

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import Modal from 'react-modal'
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
 
-import { Container, Content } from './styles'
+import { Container, Content } from './styles';
 
 interface HeaderProps {
   onOpenNewTransactionModal: ()  => void;
